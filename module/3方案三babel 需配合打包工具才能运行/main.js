3d504bf23dc766e7a86f11a909999f22bc763775
shengxia <15754600159@163.com>
2017-09-13 10:40:28 +0800

    日常添加

--- module/3方案三babel 需配合打包工具才能运行/main.js
+++
@@ -1,6 +0,0 @@
-// import {firstName, lastName, year} from './profile';
-alert('aaa')
-import {name as nameA} from 'a';
-import {name as nameB} from 'b';
-
-console.log(nameA);
--- 模拟桌面鼠标左键选中区域/index.js
+++ 模拟桌面鼠标左键选中区域/index.js
@@ -3,167 +3,91 @@
     document.onmousedown = function () {
 
         var selList = [];
-
         var fileNodes = document.getElementsByTagName("div");
-
         for (var i = 0; i < fileNodes.length; i++) {
-
             if (fileNodes[i].className.indexOf("fileDiv") != -1) {
-
                 fileNodes[i].className = "fileDiv";
-
                 selList.push(fileNodes[i]);
-
             }
-
         }
 
         var isSelect = true;
-
         var evt = window.event || arguments[0];
-
-        var startX = (evt.x || evt.clientX);
-
-        var startY = (evt.y || evt.clientY);
-
+        var startX = (evt.x || evt.clientX); //设置或获取鼠标指针位置相对于父文档的 x 像素坐标
+        var startY = (evt.y || evt.clientY); //设置或获取鼠标指针位置相对于父文档的 y 像素坐标
         var selDiv = document.createElement("div");
-
         selDiv.style.cssText = "position:absolute;width:0px;height:0px;font-size:0px;margin:0px;padding:0px;border:1px dashed #0099FF;background-color:#C3D5ED;z-index:1000;filter:alpha(opacity:60);opacity:0.6;display:none;";
-
         selDiv.id = "selectDiv";
-
         document.body.appendChild(selDiv);
-
         selDiv.style.left = startX + "px";
-
         selDiv.style.top = startY + "px";
-
         var _x = null;
-
         var _y = null;
-
-        clearEventBubble(evt);
+        clearEventBubble(evt);//阻止事件冒泡
 
         document.onmousemove = function () {
-
             evt = window.event || arguments[0];
-
             if (isSelect) {
-
                 if (selDiv.style.display == "none") {
-
                     selDiv.style.display = "";
-
                 }
-
                 _x = (evt.x || evt.clientX);
-
                 _y = (evt.y || evt.clientY);
-
                 selDiv.style.left = Math.min(_x, startX) + "px";
-
                 selDiv.style.top = Math.min(_y, startY) + "px";
-
                 selDiv.style.width = Math.abs(_x - startX) + "px";
-
                 selDiv.style.height = Math.abs(_y - startY) + "px";
 
                 // ---------------- 关键算法 ---------------------  
-
                 var _l = selDiv.offsetLeft, _t = selDiv.offsetTop;
-
                 var _w = selDiv.offsetWidth, _h = selDiv.offsetHeight;
-
                 for (var i = 0; i < selList.length; i++) {
-
                     var sl = selList[i].offsetWidth + selList[i].offsetLeft;
-
                     var st = selList[i].offsetHeight + selList[i].offsetTop;
-
                     if (sl > _l && st > _t && selList[i].offsetLeft < _l + _w && selList[i].offsetTop < _t + _h) {
-
                         if (selList[i].className.indexOf("seled") == -1) {
-
                             selList[i].className = selList[i].className + " seled";
-
                         }
-
                     } else {
-
                         if (selList[i].className.indexOf("seled") != -1) {
-
                             selList[i].className = "fileDiv";
-
                         }
-
                     }
-
                 }
-
             }
 
             clearEventBubble(evt);
-
         }
-
         document.onmouseup = function () {
-
             isSelect = false;
-
             if (selDiv) {
-
                 document.body.removeChild(selDiv);
-
                 showSelDiv(selList);
-
             }
-
             selList = null, _x = null, _y = null, selDiv = null, startX = null, startY = null, evt = null;
-
         }
-
     }
-
 })();
 
 function clearEventBubble(evt) {
-
     if (evt.stopPropagation)
-
         evt.stopPropagation();
-
     else
-
         evt.cancelBubble = true;
-
     if (evt.preventDefault)
-
         evt.preventDefault();
-
     else
-
         evt.returnValue = false;
-
 }
 
 function showSelDiv(arr) {
-
     var count = 0;
-
     var selInfo = "";
-
     for (var i = 0; i < arr.length; i++) {
-
         if (arr[i].className.indexOf("seled") != -1) {
-
             count++;
-
             selInfo += arr[i].innerHTML + "\n";
-
         }
-
     }
-
     alert("共选择 " + count + " 个文件，分别是：\n" + selInfo);
-
 } 
