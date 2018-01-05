694f0301f1e51e16aedbeaf9bbbddf300f09fbc2
shengxia <15754600159@163.com>
2018-01-05 18:50:02 +0800

    aaa

--- 03 有趣/00 前端开发框架/02 webpack/app/js/app.js
+++ 03 有趣/00 前端开发框架/02 webpack/app/js/app.js
@@ -1,5 +1,10 @@
-// 引入相关资源JS、CSS
-import './header.js';
+// css
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+// 引入mine的JS、CSS
+import '../css/main.css';//使用require导入css文件
+import '../less/main.less';
 
 $(function() {
     
--- 03 有趣/00 前端开发框架/02 webpack/app/libs/libs.js
+++
@@ -1,6 +0,0 @@
-// css
-import 'bootstrap/dist/css/bootstrap.css'
-
-// const jQuery = require('jquery'); //jquery插件在plugins中引入
-import 'bootstrap/dist/js/bootstrap.js'
-const echarts = require('echarts');
