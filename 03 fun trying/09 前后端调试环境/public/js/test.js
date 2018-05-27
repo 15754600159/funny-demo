7a71daaaa04e0ac6e3f31d6b3b0d4079282ea468
shengshunyan <shengshunyan@mininglamp.com>
2018-05-27 15:54:48 +0800

    add ## 你不知道的JavaScript

--- 03 fun trying/09 前后端调试环境/public/js/test.js
+++ 03 fun trying/09 前后端调试环境/public/js/test.js
@@ -1,19 +1,14 @@
-$(function () {
+// $(function () {
 
-    const input = document.querySelector('input'),
-        eventObj = document.createEvent('Events');
-    let cpLock = false;
-    eventObj.initEvent('input', false, true);
-    input.addEventListener('compositionstart', () => {
-        cpLock = true;
-    });
-    input.addEventListener('compositionend', () => {
-        cpLock = false;
-        input.dispatchEvent(eventObj); // 针对chrome的
-    });
-    input.addEventListener('input', (e) => {
-        if (cpLock) return;
-        console.log(11);
-    });
 
-})
+// })
+
+
+var Foo = {
+    baz: function baz() {
+        if (x < 10) {
+            return baz( x * 2 );
+        }
+        return x;
+    }
+}
