159070ff582a2ae451088d0049da04a03492ee53
shengshunyan <shengshunyan@mininglamp.com>
2019-12-12 10:10:29 +0800

    delete project

--- 03 fun trying/00 前端开发框架/04 webpack_mininglamp/server/rewriterJSON.js
+++
@@ -1,5 +0,0 @@
-const router = {
-    '/api/test': '/test'
-}
-
-module.exports = router
--- 03 fun trying/00 前端开发框架/04 webpack_mininglamp/src/login/actions/actionTypes.js
+++
@@ -1,8 +0,0 @@
-/**
- * 功能：动作名称
- * 作者：安超
- * 日期： 2018/3/19
- */
-
-export const SET_USER_INFO_LOGIN = 'SET_USER_INFO_LOGIN'
-export const USER_TYPE = ['0', '1']
